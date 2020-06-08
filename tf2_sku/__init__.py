b2331a1208b1a609b45e34c4e86a2f9004a6ccd6
offish <overutilization@gmail.com>
2020-06-08 20:12:52 +0200

    Create __init__.py

---
+++ tf2_sku/__init__.py
@@ -0,0 +1,15 @@
+"""
+tf2_sku
+=======
+Format TF2 items as strings or objects.
+"""
+
+
+__title__ = "tf2_sku"
+__author__ = "offish"
+__license__ = "MIT"
+__version__ = "1.0.0"
+
+
+from .sku import to_sku, from_sku
+from .prettify import prettify
