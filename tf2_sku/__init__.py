bee71f207d753a35ae08d18b9e15134cff6bc907
offish <overutilization@gmail.com>
2021-11-10 19:47:38 +0100

    update version

--- tf2_sku/__init__.py
+++ tf2_sku/__init__.py
@@ -8,7 +8,7 @@ Format TF2 items as strings or objects.
 __title__ = "tf2_sku"
 __author__ = "offish"
 __license__ = "MIT"
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 
 
 from .sku import to_sku, from_sku
